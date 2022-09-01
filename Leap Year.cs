44b1ed9942df0a1caf15007c5b696b80770c0162
ini10000 <iniobong10000@gmail.com>
2022-09-01 21:49:28 +0100

    Fixed leap year code

--- Leap Year.cs
+++ Leap Year.cs
@@ -8,13 +8,13 @@ namespace App {
             
             Console.WriteLine("The next 20 leap years from the given year " + year + " are: ");
             int count = 0;
-    while(count != 20){
-    year = year+4;
-    if((year%400==0) || (year%4==0 && year%100 != 0)){
-        Console.WriteLine(year);
-    }
-    count++;
-    }
+            while(count != 20){
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+                    Console.WriteLine(year);
+                    count++;
+                }
+                year++;
+            }
         }
     }
 }
